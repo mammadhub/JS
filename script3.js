d4d6d55797ccf7723177df191dec0a2b6a41838b
mammad <memmed2512@gmail.com>
2024-04-23 14:54:21 +0400

    js

--- script3.js
+++ script3.js
@@ -1,11 +1,13 @@
-function A(x){
-    let cem=0;
-    let say=0;
-    for(let i=0;i<x.length; i++){
-        cem=cem+x[i];
-        say++;
+function A(x) {
+    let cem = 0;
+    let say = 0;
+    for (let i = 0; i < x.length; i++) {
+        if (typeof x[i] == 'number' ) {
+            cem = cem + x[i];
+            say++;
+        }
     }
-    return cem/say;
+    return cem / say;
 }
-let x=[12, 12,14, 20,10,12,10,6];
+let x = [12, 12, 14, 20, 'a',10, 12, 10, 'd','6'];
 console.log(A(x));
